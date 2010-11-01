ac9e0797d14ffcde35051a435dea128968535209
jsmadja <jsmadja@xebia.fr>
2010-11-01 15:58:19 +0100

    Add Maven pom.xml and initial files (dummy files to verify compilation, dependencies and packaging)

---
+++ src/main/java/com/xebia/devradar/domain/Radar.java
@@ -0,0 +1,8 @@
+package com.xebia.devradar.domain;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Radar {
+
+}
---
+++ src/test/java/com/xebia/devradar/DummyTest.java
@@ -0,0 +1,13 @@
+package com.xebia.devradar;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DummyTest {
+
+	@Test
+	public void method() {
+		assertTrue(true);
+	}
+}
