f71da874aba3561fcd4c4144dbe64bd75aa470a7
mrenou <mrenou@xebia.fr>
2011-02-16 13:38:28 +0100

    add hudson factory test for json media type

--- src/test/java/com/xebia/devradar/HudsonFetcherFactoryTest.java
+++ src/test/java/com/xebia/devradar/HudsonFetcherFactoryTest.java
@@ -2,6 +2,12 @@ package com.xebia.devradar;
 
 import org.junit.Test;
 
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.ext.MessageBodyReader;
+import javax.ws.rs.ext.MessageBodyWriter;
+
+import java.lang.annotation.Annotation;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
@@ -25,6 +31,22 @@ public class HudsonFetcherFactoryTest {
         assertThat(fetcher.url, equalTo(buildsUrl));
         assertThat(fetcher.hudsonUrl, equalTo(hudsonUrl));
         assertThat(fetcher.client.getProviders(), notNullValue());
+
+    }
+
+    @Test
+    public void should_client_get_a_message_body_reader() {
+        //given
+        String hudsonUrl = "http://hudson.url.org/hudson";
+        String jobName = "hudsonJob";
+        HudsonFetcherFactory factory = new HudsonFetcherFactory();
+
+        //when
+        HudsonFetcher fetcher = factory.getHudsonFetcher(hudsonUrl, jobName);
+
+        //then
+        String buildsUrl = hudsonUrl + "/job/" + jobName + "/api/json";
+        assertThat(fetcher.client.getProviders().getMessageBodyWriter(HudsonBuildsDTO.class, HudsonBuildsDTO.class, new Annotation[] {}, MediaType.APPLICATION_JSON_TYPE), notNullValue());
     }
 
 }
